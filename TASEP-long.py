c01f116d6cdde4947d8321e8fbe64ce767baee86
Ilja List <moska.viz@gmx.de>
2023-10-16 15:40:13 +0200

    clean up

--- TASEP-long.py
+++ TASEP-long.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import time
 import TASEP
 import pandas as pd 
 
@@ -9,13 +10,13 @@ iterations = 50*1000
 Runs = 100   
 density = np.zeros((Runs, iterations))
 
+# create CSV
 title = '{}-{}-{}-{}-{}.csv'.format(*(100*probs).astype(int), L)
-
 with open(title, 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #filewriter.writerow(['N', 'Density_values'])
 
+# perform various simulations and saving the density in a .csv file
 for r in range(Runs):
     print(r)
     density[r] = TASEP.update(iterations, L, TASEP.init_state(L), probs)
