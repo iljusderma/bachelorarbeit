e949c153f5855e8ca6f859285027e3717bab75b9
Ilja List <moska.viz@gmx.de>
2023-10-16 15:30:46 +0200

    mean over all different simulations

--- TASEP-long.py
+++ TASEP-long.py
@@ -3,13 +3,11 @@ import numpy as np
 import TASEP
 import pandas as pd 
 
-probs = np.array([0.3, 0.6, 0.8, 0])
+probs = np.array([0.6, 0.3, 0.7, 0])
 L = 1000
-iterations = 10*1000
-Runs = 3
+iterations = 50*1000
+Runs = 100   
 density = np.zeros((Runs, iterations))
-
-state = TASEP.init_state(L)
 
 title = '{}-{}-{}-{}-{}.csv'.format(*(100*probs).astype(int), L)
 
@@ -20,7 +18,7 @@ with open(title, 'w', newline='') as csvfile:
 
 for r in range(Runs):
     print(r)
-    density[r] = TASEP.update(iterations, L, state, probs)
+    density[r] = TASEP.update(iterations, L, TASEP.init_state(L), probs)
 
 df = pd.DataFrame(density)
-df.to_csv(title, header=False)
+df.to_csv(title, header=False, index=False)
---
+++ TASEP-plot-mean.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def get_rates(string):
+    return np.array(string.split('/')[-1][:-4].split('-')[:-1]).astype(float)/100
+
+# read csv
+path = "/home2/ilja/Documents/Bachelorarbeit/bachelorarbeit/30-60-80-0-1000.csv"
+df = pd.read_csv(path, header=None, index_col=False)
+density = df.to_numpy()
+# calculate the mean over all rows
+average_density = np.mean(density, axis=0)
+rates = get_rates(path)
+print(rates)
+iterations = 50*1000
+
+
+
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(111, title='Evolution of density', xlabel='iterations', ylabel='density')
+ax.plot(np.arange(len(average_density)), average_density, marker=',', lw=0)
+if rates[0] > rates[1] and rates[1] < 0.5:
+    ax.plot(np.full((iterations,), 1-rates[1]), color='red', label='HD')
+if rates[0] < rates[1] and rates[0] < 0.5:
+    ax.plot(np.full((iterations,), rates[0]), color='red', label='LD')
+elif rates[0] > 0.5 and rates[1] > 0.5:
+    ax.plot(np.full((iterations,), 0.5), color='red', label='MC')
+else:
+    pass
+ax.legend()
+plt.show()
