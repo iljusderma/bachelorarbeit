1e6f9c3f1702d369cf090d7d85877be4645aa37e
Ilja List <55060760+iljusderma@users.noreply.github.com>
2023-10-16 13:43:53 +0200

    test

--- mean-TASEP.py
+++ mean-TASEP.py
@@ -4,3 +4,5 @@ with open('persons.csv', 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(['alpha', 'beta', 'p', 'q', 'iterations', 'density_values'])
+
+print('hello')
