7f50d1f1477c3cf26972a1b28d297bd8d441e0f6
Ilja List <moska.viz@gmx.de>
2023-10-18 11:47:47 +0200

    small bug fix

--- TASEP-long.py
+++ TASEP-long.py
@@ -4,33 +4,29 @@ import TASEP
 import pandas as pd
 
 def save_data(data, name):
+    # create CSV
     title = '{}-{}-{}-{}-{}-' + name + '.csv'.format(*(100*rates).astype(int), L)
     with open(title, 'w', newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    # save data to CSV
     data_df = pd.DataFrame(data)
     data_df.to_csv(title, header=False, index=False)
-    
 
 rates = np.array([0.6, 0.3, 0.7, 0])
 L = 1000
 iterations = 50*1000
-Runs = 100   
+Runs = 100
+current_stepsize = 100
 density = np.zeros((Runs, iterations))
-current = np.zeros((Runs, iterations))
+current = np.zeros((Runs, (iterations - 1)//current_stepsize))
 
-# create CSV
-title = '{}-{}-{}-{}-{}-current.csv'.format(*(100*rates).astype(int), L)
-with open(title, 'w', newline='') as csvfile:
-    filewriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-# perform various simulations and saving the density in a .csv file
+# perform various simulations
 for r in range(Runs):
     print(str(round(r/Runs, 2)) + '%')
     chain = TASEP.Chain(L, rates)
     chain.initialize_state()
-    density[r], current[r] = TASEP.iterate(iterations, chain)
+    density[r], current[r] = TASEP.iterate(iterations, chain, current_stepsize)
 
 save_data(density, [ i for i, j in locals().items() if j == density][0])
 save_data(current, [ i for i, j in locals().items() if j == current][0])
