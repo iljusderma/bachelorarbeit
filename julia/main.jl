6909ef257216b7c9b206e081276664c6ea3e3289
iljusderma <il214@gmx.net>
2023-11-09 11:48:09 +0100

    first profile analysis -> delete snapshots

--- julia/main.jl
+++ julia/main.jl
@@ -1,4 +1,4 @@
-using Random, Plots, Statistics, PlotThemes, LaTeXStrings, ProfileView
+using Random, Plots, Statistics, PlotThemes, LaTeXStrings
 theme(:lime)
 
 function initialize_state(L)
@@ -6,6 +6,38 @@ function initialize_state(L)
     state = zeros(L)
     state[1:Int(floor(L*0.5))] .= 1
     return shuffle(state)
+end
+
+function pole_update(state, hop_counter, α, β, p1, p2)
+    L = length(state)
+    site = rand(0:L)
+    if site == 0
+        if state[1] == 0 && rand() < α
+            # injection
+            state[1] += 1
+        end
+    elseif site == L 
+        if state[L] == 1 && rand() < β
+            # ejection
+            state[L] -= 1
+        end
+    elseif site == L/2 + 1
+        if state[site] == 1 && state[site + 1] == 0 && rand() < p2
+            # hop to right with p2
+            state[site + 1] += 1
+            state[site] -= 1
+        end
+    else
+        if state[site] == 1 && state[site + 1] == 0 && rand() < p1
+            # hop to right
+            state[site + 1] += 1
+            state[site] -= 1
+            if site == L/2
+                hop_counter += 1
+            end
+        end
+    end
+    return state, hop_counter
 end
 
 function update(state, hop_counter, α, β, p)
@@ -34,7 +66,7 @@ function update(state, hop_counter, α, β, p)
     return state, hop_counter
 end
 
-function simulate(α, β, L, t0, p=1)
+function simulate(α, β, L, t0, p1=1, p2=0.8)
     # initialize state
     state = initialize_state(L)
     # save state history in all_states every L-th time step
@@ -44,12 +76,10 @@ function simulate(α, β, L, t0, p=1)
     hop_counter = 0
     # perform L*t0 update steps
     for t in 1:(t0*L)
-        state, hop_counter = update(state, hop_counter, α, β, p)
+        state, hop_counter = pole_update(state, hop_counter, α, β, p1, p2)
         # save snapshot
         if t%L == 0
-            STATES[:, div(t, L)] = vec(mean(snapshot, dims=2))
-        else
-            snapshot[:, t%L] = state 
+            STATES[:, div(t, L)] = state
         end
     end
     return STATES, hop_counter/10_000
--- julia/simple-plot.jl
+++ julia/simple-plot.jl
@@ -1,30 +1,34 @@
 include("main.jl")
+using ProfileView
 theme(:lime)
 
 # initialize lattice parameters
 # lattice size L, injection rate α, ejection rate β, hop rate p
-t0 = 2_000 # one time unit includes L updates of the lattice
-L = 300
+t0 = 100_000 # one time unit includes L updates of the lattice
+L = 500
 α = 0.3
-β = 0.3
-p = 1
+β = 0.8
+p1 = 1
+p2 = 1
 
 # perform update
-STATES, curr = simulate(α, β, L, t0)
-
+@time begin
+#ProfileView.@profview
+STATES, curr = simulate(α, β, L, t0, p1, p2)
+end
 # plot data
 cut_STATES = STATES[:, 2500:t0]
 densityprofile = vec(mean(cut_STATES, dims=2))
 totaldensity = vec(mean(STATES, dims=1))
 
-#=
-scatter(densityprofile, ms=1, 
+
+scatter(totaldensity, ms=1, 
     label="α=$α, β=$β, p=$p", 
     ylims=[0, 1], 
     title="Density profile",
     ylabel=L"\langle \rho \rangle", 
     xlabel="Lattice site i")
 hline!([0.7], label="Expected density in HD phase")
-hline!([0.3], label="Expected density in LD phase")=#
+hline!([0.3], label="Expected density in LD phase")
 
-heatmap(STATES', ylabel="Time t", xlabel="Lattice site i")
+#heatmap(STATES', ylabel="Time t", xlabel="Lattice site i")
